1f8f272bd9728d6ad9979f04c0c8b2e51198a604
Pixelmaster93 <andy.toro93@gmail.com>
2024-02-06 12:54:55 +0100

    ok

--- TestAppConsole/Program.cs
+++ TestAppConsole/Program.cs
@@ -14,59 +14,51 @@ GameDal gamesDal =
     );
 
 //Vado a creare le tabelle
-//gamesDal.CreateAllTables();
+gamesDal.CreateAllTables();
 
 
-//int affected =
-//    gamesDal
-//    .AddNewGame
-//        (
-//            new Game
-//            (
-//                "zelda-botw",
-//                "The Legend of Zelda Breath of the Wild",
-//                "The best Zelda of all time?",
-//                "zelda;nintendo;gdr;adevnture"
-//            )
-//        );
+int affected =
+    gamesDal
+    .AddNewGame
+        (
+            new Game
+            (
+                "zelda-botw",
+                "The Legend of Zelda Breath of the Wild",
+                "The best Zelda of all time?",
+                "zelda;nintendo;gdr;adevnture"
+            )
+        );
 
-//    affected +=
-//    gamesDal
-//    .AddNewGame
-//        (
-//            new Game
-//            (
-//                "elden-ring",
-//                "Elden Ring",
-//                "GOTY 2022",
-//                "soulslike;gdr;adevnture"
-//            )
-//        );
+affected +=
+gamesDal
+.AddNewGame
+    (
+        new Game
+        (
+            "elden-ring",
+            "Elden Ring",
+            "GOTY 2022",
+            "soulslike;gdr;adevnture"
+        )
+    );
 
-//Console.WriteLine($"Added {affected} game(s)");
-
-//Console.Write("Vuoi vedere tutti i giochi sul DB?(SI/NO) ");
-//string reponse = Console.ReadLine().ToString().ToUpper();
-
-//if (reponse == "SI")
-//{
-//    var allGames = gamesDal.GetAllGames();
-//    foreach (var game in allGames)
-//        Console.WriteLine(game);
-//}
+Console.WriteLine($"Added {affected} game(s)");
 
 
-//Console.WriteLine("Inserire nome del gioco!");
-//string partialname = Console.ReadLine();
-//Console.WriteLine("Inserire il tag!");
-//string tags = Console.ReadLine();
 
-//var games = gamesDal.GetGamesByPartialName(null,null);
-//foreach (var game in games)
-//    Console.WriteLine(game);
+var allGames = gamesDal.GetAllGames();
+foreach (var game in allGames)
+    Console.WriteLine(game);
 
-//Console.WriteLine("Cancello il file!");
-//File.Delete("..\\..\\Data\\test.db");
+
+
+
+var games = gamesDal.GetGamesByPartialName(null, null);
+foreach (var game in games)
+    Console.WriteLine(game);
+
+
 
 
 gamesDal.DropAllTables();
